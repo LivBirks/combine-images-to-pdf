e38b9c5ba4ca6071a36391ff958243d50d9dd77e
LivBirks <92850547+LivBirks@users.noreply.github.com>
2023-08-25 17:30:03 +0100

    Added main code

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import os
 import img2pdf
 
-print("hi")
+directory_path = input("Please enter the path of the directory containing your JPEG images: ")
+pdf_file_name = input("Please give your PDF file a name (e.g. output.pdf): ")
+
+image_files = [i for i in os.listdir(directory_path) if i.endswith(".jpg")]
+
+pdf_data = img2pdf.convert(image_files)
+
+with open("output.pdf", "wb") as file:
+    file.write(pdf_data)
