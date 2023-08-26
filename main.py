41f37768940011d0fefe50a88b9fa123479b7b28
LivBirks <92850547+LivBirks@users.noreply.github.com>
2023-08-26 09:58:46 +0100

    Finished script

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import os
 import img2pdf
 
-directory_path = input("Please enter the path of the directory containing your JPEG images: ")
-pdf_file_name = input("Please give your PDF file a name (e.g. output.pdf): ")
 
-image_files = [i for i in os.listdir(directory_path) if i.endswith(".jpg")]
+def get_images(path):
+    return [i for i in os.listdir(path) if i.endswith(".jpg")]
 
-pdf_data = img2pdf.convert(image_files)
 
-with open("output.pdf", "wb") as file:
-    file.write(pdf_data)
+def convert_images_to_pdf_data(images):
+    return img2pdf.convert(images)
+
+
+def write_pdf_file(pdf_file, data):
+    with open(pdf_file, "wb") as file:
+        file.write(data)
+
+
+print("Fetching image files...")
+image_files = get_images("./")
+
+print("Converting images to PDF...")
+pdf_data = convert_images_to_pdf_data(image_files)
+
+pdf_file_name = input("Please enter the name of your output PDF file (e.g. output.pdf): ")
+
+print(f"Copying to {pdf_file_name}...")
+write_pdf_file(pdf_file_name, pdf_data)
+print(f"Successfully converted images to {pdf_file_name}.")
+
